512a390175e9bf12b5a03272a0b2b0e1cee60b9a
vuducdu86 <69500569+vuducdu86@users.noreply.github.com>
2023-11-21 09:26:20 +0700

    Update blueTooth_utrasonicControlCar.ino

--- blueTooth_utrasonicControlCar.ino
+++ blueTooth_utrasonicControlCar.ino
@@ -24,8 +24,8 @@ boolean debounceButton(boolean state) // Khai báo biến dội phím nút nhấ
   }
 
 int pressed = 0; // nút nhấn đã được nhấn kiểu interger
-int val;         // value kiểu interger
-int cm = 0;      // khoảng cách đến vật cản kiểu interger, giá trị ban đâu là 0
+int val;         // biến điều khiển bluetooth kiểu interger
+int cm = 0;      // khoảng cách đến vật cản theo đơn vị cm
 int duration=0;  // thời gian từ cảm biến đến vật cản
 int iterations=5;// số lần thực hiện gói ping
 int distance = 0;// khoảng cách từ cảm biến đến vật cản cả đi và về
@@ -56,7 +56,7 @@ void setup()
   delay(2000); // Delay 2 giây
 }
 
-void loop()
+void loop()// Chương trình lặp chính
 {
 //  Serial.println("Press button to select control");
 //  lcd.setCursor(0,0);
@@ -66,67 +66,67 @@ void loop()
     pressed++;    // Biến ĐÃ NHẤN tăng lên 1 giá trị
     buttonState=LOW;  //Trạng thái nút nhấn về 0
     }
-    else if(debounceButton(buttonState) == HIGH && buttonState == LOW)
+    else if(debounceButton(buttonState) == HIGH && buttonState == LOW)//Nếu ngược lại thì trạng thái nút nhấn là 1
     {
       buttonState = HIGH;
       }
-  if(pressed==0){
+  if(pressed==0){                //Nếu chưa nhấn nút nhấn thì hiển thị Bấm nút để chọn
     Serial.println("Press button to select control");
     lcd.setCursor(0,0);
     lcd.print("Bam nut de chon");
   }
-  if(pressed==1)
+  if(pressed==1)                // Nếu nhấn lần 1 thì hiển thị Chương trình Xe tránh vật cản
   {
     Serial.println("Obstacle Advoiding Robot");
     lcd.setCursor(0,0);
     lcd.print("Xe Tranh Vat Can");
     obstacleAdvoiding();
     }
-  if(pressed==2)
+  if(pressed==2)                // Nếu nhấn lần 2 thì hiển thị chạy chương trình xe điều khiển bằng bluetooth
   {
     Serial.println("Bluetooth Control Car");
     lcd.setCursor(0,0);
     lcd.print("Xe Bluetooth    ");
     bluetoothCar();
   }
-   if(pressed>=3)
+   if(pressed>=3)               // Nếu nhấn lần 3 thi xóa LCD, trở về trạng thái dừng
   {
     pressed=0;
     lcd.clear();
     }
  }
 
-void obstacleAdvoiding()
+void obstacleAdvoiding()     //Chương trình xe tự tránh vật cản bằng cảm biến siêu âm
 {
-  distance = readPing();
-  Serial.print("Distance Ahead is: ");
+  distance = readPing(); // Gán biến khoảng cách đến vật cản bằng cách đọc giá trị cảm biến
+  Serial.print("Distance Ahead is: ");  // In giá trị khoảng cách ra serial monitor
   Serial.println(distance);
-  if (distance <= 20)
+  if (distance <= 20)  // Nếu khoảng cách đến vật cản <20 cm
   {
-    moveStop();
-    delay(50);
-    moveBackward();
-    delay(400);
-    moveStop();
-    delay(50);
-    distanceRight = lookRight();
-    Serial.print("Distance Right is: ");
+    moveStop(); // Dừng lại
+    delay(50);  
+    moveBackward(); // Chạy chương trình con "Lùi lại"
+    delay(400);  
+    moveStop();  // Dừng lại
+    delay(50); 
+    distanceRight = lookRight(); // Khoảng cách phải bằng đọc giá trị chương trình con "Nhìn Phải"
+    Serial.print("Distance Right is: ");  // In ra giá trị Khoảng cách bên phải là
     Serial.print(distanceRight);
     Serial.print("/ ");
     delay(300);
-    distanceLeft = lookLeft();
-    Serial.print("Distance Left is: ");
+    distanceLeft = lookLeft();  // Khoảng cách trái bằng đọc giá trị chương trình con "Nhìn Trái"
+    Serial.print("Distance Left is: ");  // In ra giá trị Khoảng cách bên trái là
     Serial.println(distanceLeft);
     Serial.println("");
     delay(300);
 
-    if (distanceRight >= distanceLeft)
+    if (distanceRight >= distanceLeft)  // Nếu khoảng cách Phải > Khoảng cách trái thì Rẽ Phải, rồi Dừng lại, không xoay vòng
     {
       turnRight();
       delay(700);
       moveStop();
     }
-    else
+    else                               // Nếu Khoảng cách Phải < Khoản cách trái thì Rẽ trái rồi Dừng lại, không xoay vòng
     {
       turnLeft();
       delay(760); 
@@ -135,103 +135,103 @@ void obstacleAdvoiding()
   }
   else
   {
-    moveForward();
+    moveForward();           // Nếu khoảng cách đến vật cản > 20 cm thì đi Thẳng
   }
 }
 
-void bluetoothCar()
+void bluetoothCar()  // Chương trình điều khiển bằng Bluetooth
 {
-  while (Serial.available() > 0)
+  while (Serial.available() > 0) // Vòng lặp điều kiện trước: Nếu giá trị trên chân nối tiếp kết nối vào module bluetooth khác không
   {
-    val = Serial.read();
+    val = Serial.read();  // Biến đọc giá trị điều khiển bằng app
   }
-  if( val == 'F') //Forward
+  if( val == 'F') //Nếu nhấn F thì đi thẳng 
     {
       moveForward();  
     }
-    else if(val == 'B') //Backward
+    else if(val == 'B') //Nếu nhấn B thì đi lùi
     {
       moveBackward();
     }  
-    else if(val == 'L') //Left
+    else if(val == 'L') //Nếu nhấn L thì rẽ trái
     {
       turnLeft();
     }
-    else if(val == 'R') //Right
+    else if(val == 'R') //nếu nhấn R thì rẽ phải
     {
       turnRight();
     }    
-    else if(val == 'S') //Stop
+    else if(val == 'S') //Nếu nhấn S thì Dừng
     {
       moveStop();
     }
-    else if(val == 'I') //Forward Right
+    else if(val == 'I') //Nếu nhấn I thì tiến phải
     {
       forwardRight();
     }
-    else if(val == 'J') //Backward Right
+    else if(val == 'J') //Nếu nhấn J thì lùi Phải
     {
       backwardRight();
     }
-    else if(val == 'G') //Forward Left
+    else if(val == 'G') //Nếu nhấn G thì Tiến trái
     {
       forwardLeft();
     }
-    else if(val == 'H') //Backward Left
+    else if(val == 'H') //Nếu nhấn H thì Lùi trái
     {
       backwardLeft();
     }
 }
-
+// Chương trình con đọc giá trị cảm biến siêu âm
 int readPing()
 {
   delay(70);
-  duration = sonar.ping_median(iterations);
-  cm=(duration/2)*0.0343;
-  if(cm>=400)
+  duration = sonar.ping_median(iterations);  // Đo khoảng thời gian bằng cách ping 5 lần
+  cm=(duration/2)*0.0343;// khoảng cách đến vật cản = Thời gian * vận tốc âm thanh theo đơn vị cm/micro giây
+  if(cm>=400)  // Nếu khoảng cách đến vật cản > 400 cm thì giá trị khoảng cách là 400cm vì giá trị tối đa mà cảm biến hoạt động được
   {
     cm=400;
   }
   if(cm<=2)
   {
-    cm=400;
+    cm=400;   //Nếu khoảng cách đến vật cản < 2 cm thì giá trị khoảng cách là 2cm vì giá trị tối thiểu mà cảm biến hoạt động được
   }
-  return cm;
+  return cm; // Trả về giá trị khoảng cách
 }
-
+// Chương trình con Nhìn Phải
 int lookRight()
 {  
-  servo.write(9);
+  servo.write(9); // Chân server kết nối đến cổng 9 của aduino
   delay(500);
-  distance = readPing();
+  distance = readPing(); // Đọc giá trị cảm biến siêu âm
   delay(100);
-  return distance;
+  return distance; trả về giá trị khoảng cách
   delay(100);
   } 
 
-int lookLeft()
+int lookLeft()  //Chương trình con " nhìn trái
 {
-  servo.write(179);
+  servo.write(179);  // Quay server 179 độ
   delay(500);
   distance = readPing();
   delay(100);
-  servo.write(87);
+  servo.write(87);  // Quay server 87 độ
   return distance;
   delay(100);
   }
 
-void moveForward()
+void moveForward()  //Chương trình con đi thẳng
 {
   Serial.println("Move Forward");
   lcd.setCursor(0,1);
   lcd.print("Chay Toi        ");
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);  
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW); 
+  digitalWrite(in1, HIGH);       // cho giá trị động cơ quay Tiến bên trái mức cao
+  digitalWrite(in2, LOW);        // cho giá trị động cơ quay Lùi bên trái mức thấp
+  digitalWrite(in3, HIGH);       // cho giá trị động cơ quay Tiến bên Phải mức cao
+  digitalWrite(in4, LOW);        // cho giá trị động cơ quay Lùi bên phải mức thấp
  }
 
-void moveBackward()
+void moveBackward()   // Chương trình con đi lùi
 {
   Serial.println("Move Backward");
   lcd.setCursor(0,1);
